5e0b3bbe1fdb4976450e6d493b81175c3bd52c86
husd <hu.shengdong.h@gmail.com>
2014-02-17 15:58:16 +0800

    first struct
    simple start

---
+++ src/client/client.go
@@ -0,0 +1,45 @@
+package pac_wd_src_client
+
+/*
+
+
+@date 2014/02/17
+@author husd
+@email hu.shengdong.h@gmail.com
+
+
+
+游戏的客户端，提供了游戏的主界面
+
+
+*/
+
+import(
+	"fmt"
+)
+
+func main() {
+	loadConfig()
+	startClient()
+}
+
+
+/*
+
+加载配置文件
+*/
+func loadConfig() {
+	fmt.Println("loadConfig")
+}
+
+/*
+
+开始游戏
+*/
+func startClient() {
+	fmt.Println("game start ,welcome ")
+	//检查环境，初始化页面
+	//连接服务器
+	//游戏开始
+}
+
---
+++ src/server/server.go
@@ -0,0 +1,44 @@
+package pac_wd_src_server
+/*
+
+
+@date 2014/02/17
+@author husd
+@email hu.shengdong.h@gmail.com
+
+
+
+游戏的服务端，提供了游戏的后台运算功能入口
+
+
+*/
+
+import(
+	"fmt"
+)
+
+main(){
+	loadConfig()
+	startServer()
+}
+
+/*
+
+加载配置文件
+*/
+func loadConfig() {
+	fmt.Println("loadConfig")
+}
+
+/*
+
+开始游戏
+*/
+func startServer() {
+	fmt.Println("gameserver start")
+	//TODO 初始化系统环境，加载数据
+	//
+	//监听来自客户端的连接
+
+
+}
