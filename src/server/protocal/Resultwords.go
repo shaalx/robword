3ba512757132151280f3b7dc5aded28f5521dd52
shaalx <1316011195@qq.com>
2014-03-19 22:06:27 +0800

    s

--- src/server/protocal/Resultwords.go
+++ src/server/protocal/Resultwords.go
@@ -2,12 +2,14 @@ package protocal
 
 import (
 	"encoding/json"
+	"sort"
 )
 
 type Words map[string]int
 
 type Resultwords struct {
-	Sequence string //'json:"sequence"'
+	Sequence string //'json:"sequence"
+	Score    int
 	Words    *Words //'json:"words"'
 }
 
@@ -17,4 +19,14 @@ func (r Resultwords) To_json() *([]byte) {
 		return nil
 	}
 	return &b
+}
+
+type Slice []Resultwords
+
+func (s Slice) Len() int           { return len(s) }
+func (s Slice) Less(i, j int) bool { return s[i].Score > s[j].Score }
+func (s Slice) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+
+func (s *Slice) Sort() {
+	sort.Sort(s)
 }
